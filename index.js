ba552a557de71b3df49333dfb902680375c8498b
shawonkhan1 <shawon505214@gmail.com>
2025-03-16 13:01:41 +0600

    secon time push

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ document.getElementById("btn").addEventListener('click',function(){
     }
     else {
         if(password === "123456"){
-            document.getElementById("lock").style.display = "none"
-            document.getElementById("nav").style.display = "block";
-            
+     document.getElementById("welcome").showModal();
+     document.getElementById("lock").style.display = "none"
+     document.getElementById("nav").style.display = "block";
+     
            }
+
            else{
             alert('wrong password')
            }
@@ -37,15 +39,16 @@ function allBtn(){
         
         
         data.data.forEach(elements => {
-            console.log(elements.level_no)
-        let vocabulariesSection = document.getElementById("btn-7")
+            // console.log(elements.id)
+        let vocabulariesSection = document.getElementById("btn-8")
        
         let newDiv = document.createElement("div");
     
         
         newDiv.classList.add('fle')
-        newDiv.innerHTML = `<button onclick="displayAnother(${elements.level_no})"
-         class="btn btn-outline btn-primary hover:text-black hover:bg-slate-200 "><img src="./assets/fa-book-open.png" alt=""> Lesson-${elements.level_no}</button>`
+        newDiv.innerHTML = `<button  id="btn-${elements.level_no}" onclick="displayAnother(${elements.level_no})"
+         class="btn btn-outline btn-primary hover:text-black hover:bg-slate-200 ">
+         <img src="./assets/fa-book-open.png" alt=""> Lesson-${elements.level_no}</button>`
         
         vocabulariesSection.append(newDiv);
         });
@@ -80,9 +83,10 @@ else{
     document.getElementById("no-select").style.display = "none"
 }
 data.forEach(element => {
-    console.log(element)
+    console.log(element.level_no)
+    
     let div = document.createElement("div");
-
+     
     div.innerHTML = `  
     <div class="mt-[40px]  bg-[#F8F8F8]
       p-[30px] rounded-[24px] ">
@@ -93,7 +97,7 @@ data.forEach(element => {
                  <h1 class="text-[32px] font-bold text-[#18181B]">"${element.meaning} / ${element.pronunciation}"</h1>
   
           <div class="flex justify-between p-[56px]">
-      <div class="bg-[#1A91FF1A] h-9 w-9 rounded-[12px] p-[10px]">
+      <div onclick="wordDetails(${element.id})" class="bg-[#1A91FF1A] h-9 w-9 rounded-[12px] p-[10px]">
       <img src="./assets/fa-book-open.png" alt="">
       </div>
           <div  class="bg-[#1A91FF1A] h-9 w-9 rounded-[12px] p-[10px]">
@@ -121,24 +125,55 @@ const url = `https://openapi.programming-hero.com/api/level/${id}`
 fetch(url)
 .then(res => res.json())
 .then(data=>{
-    
+    removeActive();
+    const clickbtn = document.getElementById(`btn-${id}`)
+    clickbtn.classList.add("active")
+    // console.log(clickbtn)
  display(data.data)
 })
 }
 
+// remove active class
+function removeActive() {
+    const activebtn = document.getElementsByClassName("active")
+    for(let btn of activebtn){
+        btn.classList.remove("active")
+    }
+    console.log(activebtn)
+}
 
 
+const wordDetails = (detail) => {
+    const url = `https://openapi.programming-hero.com/api/word/${detail}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+       displayWordDetail(data.data)
+    })
 
+}
 
+const displayWordDetail = (word) => {
+    document.getElementById("word_details").showModal()
+    const wordDetailsDisplay =
+     document.getElementById("wordDetailsDisplay")
+     wordDetailsDisplay.innerHTML = `
 
+      <h2>Meaning</h2>
+      <h2>${word.meaning} </h2>
 
+      <p class="mt-[5px]">Example</p>
+      <p>${word.sentence}</p>
 
-
-
-
-
-
-
-
-
+      <p class="mt-[5px]">সমার্থক শব্দ গুলো</p>
+     
+       <div class="mb-[15px]">
+       <button class="btn bg-[#EDF7FF]">${word.synonyms[0]}</button>
+        <button class="btn bg-[#EDF7FF]">${word.synonyms[1]}</button>
+        <button class="btn bg-[#EDF7FF]">${word.synonyms[2]}</button> 
+        </div>
+      
+     `
+console.log(word)
+}
 
